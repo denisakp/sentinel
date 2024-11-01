90608dad6379343a065c76ffb50ef4aad8bb4fa0
Denis AKPAGNONITE <akpagnonited@gmail.com>
2024-11-01 02:53:14 +0100

    return formatted message error

--- pkg/backup/mysql_dump/mysql_dump.go
+++ pkg/backup/mysql_dump/mysql_dump.go
@@ -57,7 +57,7 @@ func Backup(mda *MySqlDumpArgs) error {
 
 	// write backup to storage
 	if err := storageHandler.WriteBackup(stdOut.Bytes(), fullPath); err != nil {
-		return err
+		return fmt.Errorf("failed to write backup to storage - %w", err)
 	}
 
 	fmt.Printf("Backup file created at %s\n", fullPath)
